d76fb3f45335c2271b1a415e4b094b32bf79a7fc
roixa <bouxroix@gmail.com>
2014-09-03 00:22:39 +0700

    added back button

---
+++ app/src/main/java/com/tomsksummer/roix/picture3d/GLEngine/PictureRenderer.java
@@ -0,0 +1,157 @@
+package com.tomsksummer.roix.picture3d.GLEngine;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
+import android.util.Log;
+
+import com.tomsksummer.roix.picture3d.GLEngine.models3d.PictureSurface;
+import com.tomsksummer.roix.picture3d.R;
+import com.tomsksummer.roix.picture3d.interfaces.TouchSurfaceListener;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by root on 06.08.14.
+ */
+public class PictureRenderer implements GLSurfaceView.Renderer, TouchSurfaceListener {
+    private final String TAG ="PictureRenderer";
+
+    public AccelModule base;
+    public Context context;
+    private Texture box;
+    private PictureSurface pictureSurface;
+    private boolean touched;
+    private CoordHelper coord;
+
+    private boolean modeIsDraw;
+
+    public void setContext(Context context) {
+        base=new AccelModule();
+        this.context = context;
+        //cube=new Cube();
+        pictureSurface=new PictureSurface(this.context, R.drawable.nature);
+        touched=false;
+        coord=new CoordHelper(0,0,6);
+        modeIsDraw=true;
+    }
+
+
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);  // Set color's clear-value to black
+        gl.glClearDepthf(1.0f);            // Set depth's clear-value to farthest
+        gl.glEnable(GL10.GL_DEPTH_TEST);   // Enables depth-buffer for hidden surface removal
+        gl.glDepthFunc(GL10.GL_LEQUAL);    // The type of depth testing to do
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);  // nice perspective view
+        gl.glShadeModel(GL10.GL_SMOOTH);   // Enable smooth shading of color
+        gl.glDisable(GL10.GL_DITHER);      // Disable dithering for better performance
+        pictureSurface.loadTextures(gl);
+        //pictureSurface.loadTexture(gl,context);
+        //cube.loadTexture(gl, context);    // Load image into Texture (NEW)
+        gl.glEnable(GL10.GL_TEXTURE_2D);  // Enable texture (NEW)
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        if (height == 0) height = 1;   // To prevent divide by zero
+        float aspect = (float)width / height;
+
+        // Set the viewport (display area) to cover the entire window
+        gl.glViewport(0, 0, width, height);
+
+        // Setup perspective projection, with aspect ratio matches viewport
+        gl.glMatrixMode(GL10.GL_PROJECTION); // Select projection matrix
+        gl.glLoadIdentity();                 // Reset projection matrix
+        // Use perspective projection
+        GLU.gluPerspective(gl, 45, aspect, 0.1f, 100.f);
+
+        gl.glMatrixMode(GL10.GL_MODELVIEW);  // Select model-view matrix
+        gl.glLoadIdentity();                 // Reset
+
+        // You OpenGL|ES display re-sizing code here
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+
+
+        gl.glLoadIdentity();                  // Reset the current model-view matrix
+        gl.glTranslatef(-coord.camX, -coord.camY, -coord.camZ);   // Translate into the screen
+
+        if(!touched||!modeIsDraw){
+        gl.glRotatef(base.getXAngle(), 1.0f, 0.0f, 0.0f); // Rotate accel x
+        gl.glRotatef(base.getYAngle(), 0.0f, 1.0f, 0.0f); // Rotate accel y
+        gl.glRotatef(base.getZAngle(), 0.0f, 0.0f, 1.0f); // Rotate accel y
+        //
+        }
+        else  base.returnSurfaceAngle();
+
+        pictureSurface.draw(gl);
+
+
+
+
+    }
+
+
+
+    @Override
+    public void OnTouch(float x,float y) {
+        if (modeIsDraw){
+            Log.d(TAG,"on touch"+String.valueOf(x));
+            pictureSurface.touch(coord.getX(x),coord.getY(y),coord.camZ);
+        }
+
+
+
+
+
+    }
+
+    public void buttonPushedBack(){
+        pictureSurface.buttonPushedBack();
+
+    }
+
+    @Override
+    public void OnStartTouch() {
+        touched=true;
+        Log.d(TAG,"start touch");
+    }
+
+    @Override
+    public void OnEndTouch() {
+        touched=false;
+        pictureSurface.untouch();
+        Log.d(TAG,"stop touch");
+    }
+
+    public boolean  changeMode(){
+        modeIsDraw=!modeIsDraw;
+        return modeIsDraw;
+    }
+
+    public void setMode(boolean isDraw){
+        modeIsDraw =isDraw;
+    }
+
+
+    public void zoomCam(float scale){
+
+        coord.camZ+=scale;
+    }
+
+    public void moveCam(float dx,float dy){
+
+        coord.camX+=coord.getCamXAdd(dx);
+        coord.camY+=coord.getCamYAdd(dy);
+    }
+
+}
---
+++ app/src/main/java/com/tomsksummer/roix/picture3d/PictureActivity.java
@@ -0,0 +1,158 @@
+package com.tomsksummer.roix.picture3d;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.tomsksummer.roix.picture3d.GLEngine.PictureSurfaceView;
+import com.tomsksummer.roix.picture3d.GLEngine.mSurfaceView;
+import com.tomsksummer.roix.picture3d.R;
+
+public class PictureActivity extends Activity {
+
+    private PictureSurfaceView mGLSurfaceView;
+    private SensorManager sensorManager;
+    private Sensor sensorAccel;//accelerator sensor
+    private ImageButton handButton;
+    private ImageButton bendButton;
+    private ImageButton undoButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        setContentView(R.layout.activity_picture);
+        mGLSurfaceView = ( PictureSurfaceView)findViewById(R.id.surfaceView);
+        handButton=(ImageButton)findViewById(R.id.handButton);
+        bendButton=(ImageButton)findViewById(R.id.bendButton);
+        undoButton=(ImageButton)findViewById(R.id.undoButton);
+
+        View.OnClickListener buttonsListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.handButton:
+                        //handButton.setSelected(!mGLSurfaceView.changeMode());
+                        mGLSurfaceView.setMoveMode();
+                        break;
+                    case R.id.bendButton:
+                        mGLSurfaceView.setDrawMode();
+                        break;
+                    case R.id.undoButton:
+                        mGLSurfaceView.buttonPushedBack();
+                        break;
+                }
+
+            }
+        };
+
+        handButton.setOnClickListener(buttonsListener);
+        bendButton.setOnClickListener(buttonsListener);
+        undoButton.setOnClickListener(buttonsListener);
+
+    }
+
+
+    @Override
+    protected void onResume()
+    {
+        // Вызывается GL surface view's onResume() когда активити переходит onResume().
+        super.onResume();
+        sensorManager.registerListener(listener, sensorAccel,
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        // Вызывается GL surface view's onPause() когда наше активити onPause().
+        super.onPause();
+        mGLSurfaceView.onPause();
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+
+        // Be sure to call the superclass implementation
+        return super.onTouchEvent(event);
+    }
+
+
+
+    class Gesture extends GestureDetector.SimpleOnGestureListener{
+        public boolean onSingleTapUp(MotionEvent ev) {
+            return true;
+        }
+        public void onLongPress(MotionEvent ev) {
+
+        }
+        private float dist=0;
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+                                float distanceY) {
+            if(e1.getPointerCount()>1||e2.getPointerCount()>1){
+                return false;
+            }
+            dist+=distanceX;
+            Log.d("scale", "on scroll" + String.valueOf(dist));
+            return true;
+        }
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                               float velocityY) {
+            return true;
+        }
+    }
+
+
+
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            float scale = detector.getScaleFactor();
+
+            Log.d("scale ", String.valueOf(scale));
+            return true;
+        }
+    }
+
+
+    float[] valuesAccel = new float[3];
+
+
+    SensorEventListener listener = new SensorEventListener() {
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            int type=event.sensor.getType();
+            int pref=Sensor.TYPE_ACCELEROMETER;
+            if(type==pref){
+
+                for (int i = 0; i < 3; i++)    valuesAccel[i] = event.values[i];
+
+                mGLSurfaceView.renderer.base.setAcc(valuesAccel[0],valuesAccel[1],valuesAccel[2]);
+                mGLSurfaceView.requestRender();
+            }
+        }
+
+    };
+}
